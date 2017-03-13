eda5a4f15e4469f98bde11c586c4b880a84c4dd2
labertasch@googlemail.com <>
2017-03-13 16:04:00 +0100

    rewrite from POC with minimal functionality

--- README.md
+++ README.md
@@ -1,5 +1,29 @@
-# apache-sling-esx
-Experimental Sling ecma script scripting engine based on nashorn
+# Apache Sling ESX Scripting Engine
+
+# IMPORTANT
+You need to enable Nashorn support in sling.properties in order to use this 
+scripting engine.
 
 Node JS module loader for sling: https://nodejs.org/api/modules.html
 
+additionally to node_modules it will also search in esx_modules with higher priority.
+
+folllowing global module directories will be searched for:
+        - /apps/esx/node_modules
+        - /apps/esx/esx_modules
+        - /libs/esx/node_modules
+        - /libs/esx/esx_modules
+
+## Example with NODEJS Handlebars module
+'''
+var Handlebars = require("handlebars");
+
+var source = require("text!./template.html");
+
+
+var template = Handlebars.compile(source);
+
+exports.render = function() {
+	return template(currentNode);
+}
+'''
---
+++ src/main/java/org/apache/sling/scripting/esx/EsxScriptEngineFactory.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.apache.sling.scripting.esx;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import jdk.nashorn.api.scripting.NashornScriptEngineFactory;
+import org.apache.felix.scr.annotations.Activate;
+import org.apache.sling.scripting.api.AbstractScriptEngineFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.Deactivate;
+import org.apache.felix.scr.annotations.Properties;
+import org.apache.felix.scr.annotations.Property;
+import org.apache.felix.scr.annotations.Reference;
+import org.apache.felix.scr.annotations.Service;
+import org.osgi.framework.Constants;
+import org.osgi.service.component.ComponentContext;
+
+@Component(label = "ESX Scripting Engine Factory", description = "", metatype = true)
+@Service
+@Properties({
+    @Property(name = Constants.SERVICE_VENDOR, value = "The Apache Software Foundation")
+    ,
+        @Property(name = Constants.SERVICE_DESCRIPTION, value = "Scripting Engine for Ecmas Script using Node JS like module loader")
+    ,
+        @Property(name = "compatible.javax.script.name", value = "esx")
+})
+public class EsxScriptEngineFactory extends AbstractScriptEngineFactory {
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
+    private ScriptEngine nashornScriptEngine;
+    
+    @Reference
+    ScriptModuleCache moduleCache;
+
+    public EsxScriptEngineFactory() {
+        setNames("esx", "ESX");
+        setExtensions("esx", "ESX");
+    }
+
+    @Override
+    public String getLanguageName() {
+        return "ESX";
+    }
+
+    @Override
+    public String getLanguageVersion() {
+        return "1.0";
+    }
+
+    @Override
+    public ScriptEngine getScriptEngine() {
+        return new EsxScriptEngine(this);
+    }
+
+    /**
+     * 
+     * @return 
+     */
+    public ScriptModuleCache getModuleCache() {
+        return moduleCache;        
+    }
+    
+    /**
+     *
+     * @return
+     */
+    public ScriptEngine getNashornEngine() {
+        return nashornScriptEngine;
+    }
+
+    @Deactivate
+    private void deactivate() {
+        log.debug("Deactivating Engine");
+    }
+
+    @Activate
+    protected void activate(ComponentContext context) {
+        log.debug("Starting Engine");
+        // create one script engine
+        NashornScriptEngineFactory factory = new NashornScriptEngineFactory();
+        this.nashornScriptEngine = factory.getScriptEngine("-strict", "--no-java", "--no-syntax-extensions");
+        log.debug("Engine started");
+    }
+}
